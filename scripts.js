9141332e14a42cf523c4d122bb77140b467b60f7
Michelle Ferris <jmichellelim@yahoo.com>
2022-01-06 15:16:28 -0500

    finished up til part 4

--- scripts.js
+++ scripts.js
@@ -1,2 +1,35 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    document.getElementById("takeoff").addEventListener("click", function(event) {
+        let shuttleHeight = document.getElementById("spaceShuttleHeight");
+        let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (response) {
+        document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
+        document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+        shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) + 10000;
+    }
+    });
+    document.getElementById("landing").addEventListener("click", function(event) {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
+        document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = 0;
+    });
+    document.getElementById("missionAbort").addEventListener("click", function(event){
+        let response = window.confirm("Confirm that you want to abort the mission.");
+        if (response) {
+            document.getElementById("flightStatus").innerHTML = "Mission aborted.";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = 0;
+        }
+    });
+    document.getElementById("up").addEventListener("click", function(event) {
+        shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML) + 10000;
+    });
+    //document.getElementById("left").addEventListener("click", function(event) {
+    //    document.querySelector(img).style.left = 10px;
+    //})
+
+}
+window.addEventListener("load", init);
